163df068e754956483b6de63753f5e5e27c81462
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-06-24 00:24:40 +0900

    Fix: 떠돌이 상인과 거래가 가능한 이슈

--- src/main/java/com/github/silvertreekr/mCUtils/events/PreventVillagerTradeListener.java
+++ src/main/java/com/github/silvertreekr/mCUtils/events/PreventVillagerTradeListener.java
@@ -1,6 +1,7 @@
 package com.github.silvertreekr.mCUtils.events;
 
 import org.bukkit.entity.Villager;
+import org.bukkit.entity.WanderingTrader;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -9,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class PreventVillagerTradeListener implements Listener {
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-        if (event.getRightClicked() instanceof Villager) {
+        if (event.getRightClicked() instanceof Villager || event.getRightClicked() instanceof WanderingTrader) {
             event.setCancelled(true);
         }
     }
