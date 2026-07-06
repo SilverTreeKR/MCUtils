6722f8a2d1ab69052ae64bf68d53b01a409c1ce0
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-06 21:49:28 +0900

    refactor: 쿠폰 저장 로직 변경에 따른 데이터 타입 변경

--- src/main/java/com/github/silvertreekr/mcutils/dao/CouponManager.java
+++ src/main/java/com/github/silvertreekr/mcutils/dao/CouponManager.java
@@ -1,20 +1,22 @@
 package com.github.silvertreekr.mcutils.dao;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class CouponManager {
     private final CouponDAO couponDAO;
-    private final HashMap<UUID, String> userCoupons = new HashMap<>();
+    private final HashMap<UUID, Set<String>> userCoupons = new HashMap<>();
 
     public CouponManager(CouponDAO couponDAO) {
         this.couponDAO = couponDAO;
     }
 
     public CompletableFuture<Void> loadPlayerCouponData(UUID uuid) {
-        return couponDAO.getCoupons(uuid).thenAccept(strings -> {
-            userCoupons.put(uuid, strings.toString());
+        return couponDAO.getCoupons(uuid).thenAccept(coupons -> {
+            userCoupons.put(uuid, coupons);
         });
     }
 
@@ -27,7 +29,7 @@ public class CouponManager {
     }
 
     public boolean isUsedCoupon(UUID uuid, String coupon) {
-        String coupons = userCoupons.get(uuid);
+        Set<String> coupons = userCoupons.get(uuid);
         if (coupons == null) {
             return false;
         }
@@ -35,6 +37,6 @@ public class CouponManager {
     }
 
     public void useCoupon(UUID uuid, String coupon) {
-        userCoupons.put(uuid, coupon);
+        userCoupons.computeIfAbsent(uuid, k -> new HashSet<>()).add(coupon);
     }
 }
