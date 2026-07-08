d821de55dcf6edc5e754016cc16dc6954e0613fd
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-08 19:37:47 +0900

    fix: uuid를 인식할 수 없는 잠재적 문제 해결

--- src/main/java/com/github/silvertreekr/mcutils/commands/CouponCommand.java
+++ src/main/java/com/github/silvertreekr/mcutils/commands/CouponCommand.java
@@ -67,7 +67,7 @@ public class CouponCommand extends BukkitCommand {
             }
             // 아야님 전용 쿠폰 코드
             case "7H4NK54Y4" -> {
-                if (uuid.toString() != "66123349-3d00-4b75-a72a-836f1a6acf20") {
+                if (uuid.toString().equals("66123349-3d00-4b75-a72a-836f1a6acf20")) {
                     sender.sendRichMessage("<bold>[ 쿠폰 ] <reset><red>오직 아야님만 입력할 수 있는 쿠폰입니다.");
                     player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                     return false;
@@ -87,7 +87,7 @@ public class CouponCommand extends BukkitCommand {
             }
             // 종식님 전용 쿠폰 코드
             case "74HNKSJ0NG51K" -> {
-                if (uuid.toString() != "e078f0f4-8319-4ad0-8fd1-e50223218251") {
+                if (uuid.toString().equals("e078f0f4-8319-4ad0-8fd1-e50223218251")) {
                     sender.sendRichMessage("<bold>[ 쿠폰 ] <reset><red>오직 종식님만 입력할 수 있는 쿠폰입니다.");
                     player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                     return false;
@@ -107,7 +107,7 @@ public class CouponCommand extends BukkitCommand {
             }
             // 무지님 전용 쿠폰 코드
             case "7H4NK5MUZZ1" -> {
-                if (uuid.toString() != "d213a3a4-45b6-416e-af4e-15f319c11a7f") {
+                if (uuid.toString().equals("d213a3a4-45b6-416e-af4e-15f319c11a7f")) {
                     sender.sendRichMessage("<bold>[ 쿠폰 ] <reset><red>오직 무지님만 입력할 수 있는 쿠폰입니다.");
                     player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                     return false;
